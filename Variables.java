a6366070726f04d6cdaec77d3ae0358636c8514b
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-05-24 17:06:54 +0100

    Variables. declaring, assigning, logging.

---
+++ Variables.java
@@ -0,0 +1,29 @@
+// package means folder
+package java_basics;
+
+// capitalise class name
+public class Variables {
+	// need main method for code to work
+	public static void main(String[] args) {
+		
+		int x;
+		x = 4;
+		
+		// can declare and assign on same line
+		int z = 345;
+		
+		// console.log
+		System.out.println(x);
+		System.out.println(z + x);
+		
+		x = 234;
+		System.out.println(x);
+		
+		String y;
+		
+		y = "Hello java world.";
+		
+		System.out.println(y);
+		
+	}
+}

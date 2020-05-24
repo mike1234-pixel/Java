eb57490d71a66974d56c65da83196b04fb6bbcb5
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-05-24 17:45:04 +0100

    List of primitive data types.

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package java_basics;
+
+public class VariablesContinued {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 9;
+		// long data type for bigger numbers, add L to end of num
+		long bigNumber = 10000000000000L;
+		// short - max capacity = just over 32,000
+		short shortNum = 32000;
+		// byte - max capacity = 127
+		byte reallySmallNum = 127;
+		// default choice for decimal values
+		double decimalVariable = 394.001;
+		// data type float is also available
+		boolean decision = true;
+		// single character in single quotes
+		char letter = 'b';
+		
+		System.out.println(num + bigNumber + shortNum + reallySmallNum);
+	}
+
+}

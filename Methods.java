113749e29d0d66c24b0491caa6edd491344c432c
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-05-25 15:43:02 +0100

    Defining methods, invoking methods across classes.

---
+++ Methods.java
@@ -0,0 +1,61 @@
+// INVOKING METHODS in class Methods
+
+package java_basics;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello");
+		
+		// print method belongs to variable "out" which is part of 	"System" class
+		
+		// invoke method
+		// (invoking method that belongs to MyUtils class)
+	MyUtils.printSomeStuff("hello JAVA WORLD!");
+	
+	int myBigNum = MyUtils.printSomeStuff(25);
+	System.out.println(myBigNum);
+	
+	MyUtils.sum2Numbers(9, 100);
+	
+	// capturing returned result in var:
+	int myVar = MyUtils.add10(99);
+	
+	System.out.println(myVar);
+	}
+	
+	
+}
+
+// DEFINING METHODS in class MyUtils
+
+package java_basics;
+
+public class MyUtils {
+
+	// create method -- declare data type of parameter
+	// can create different methods that take different data types as arguments, 
+	// when invoking Java will figure out which version to run depending on the args you pass to it
+	public static void printSomeStuff(String message) {
+		System.out.println(message);
+	}
+	
+	public static int printSomeStuff(int number) {
+		int result = number + 5550;
+		return result;
+	}
+	
+	public static void sum2Numbers(int firstNum, int secondNum) {
+		System.out.println(firstNum + secondNum);
+	}
+	
+	// void methods cannot return a value
+	// the 'int' preceding the function name is the return type (data type to be returned)
+	public static int add10(int numeroUno) {
+		int result = numeroUno+10;
+		return result;
+	}
+}
+

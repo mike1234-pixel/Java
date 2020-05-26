71e77cc4fd06064237d9adf5266da589a5deb1bf
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-05-26 14:09:59 +0100

    Method overriding and interfaces example.

---
+++ Classes_&_Objects3____Interfaces.java
@@ -0,0 +1,99 @@
+///////////////////////// ENTRY POINT
+
+// Zoo is the entry point of the application
+
+package object_Orientation2;
+
+public class Zoo {
+
+	public static void main(String[] args) {
+		
+		Chicken chick1 = new Chicken(1, "W", 7);
+		chick1.fly();
+		
+		Sparrow sparrow1 = new Sparrow(4, "M", 7);
+		sparrow1.fly();
+	}
+
+}
+
+///////////////////////// PARENT CLASS
+
+package object_Orientation2;
+
+public class Animal {
+	
+	int age;
+	String gender;
+	int weightInLbs;
+
+	public Animal(int age, String gender, int weightInLbs) {
+		this.age = age;
+		this.gender = gender;
+		this.weightInLbs = weightInLbs;
+	}
+	
+	public void eat(){
+		System.out.println("Eating...");
+	}
+	
+	public void sleep() {
+		System.out.println("Sleeping...");
+	}
+	
+	
+}
+
+///////////////////////// CHILD CLASSES
+
+///// chicken uses method overriding
+
+package object_Orientation2;
+
+public class Chicken extends Bird {
+
+	public Chicken(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+		// TODO Auto-generated constructor stub
+	}
+	// method overriding
+	// here we're overriding/replacing a method defined in the Bird parent class
+	public void fly() {
+		System.out.println("not able to fly...");
+	}
+
+}
+
+///// sparrow uses an interface
+
+package object_Orientation2;
+
+public class Sparrow extends Bird implements Flyable {
+
+	public Sparrow(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	// An interface is a contract
+	// For any class that implements an interface, the interface wants the class to promise that it will implement that method, like below:
+	// A class can only extend one other class (have one parent), but it can have as many interfaces as it wants
+	
+	@Override
+	public void fly() {
+		System.out.println("Sparrow flying high...");
+	}
+}
+
+///////////////////////// INTERFACE
+
+package object_Orientation2;
+
+// interfaces should be named after the functionality that we are abstracting away
+public interface Flyable {
+	
+	// An interface only has "Abstract Methods"
+	// Abstract Methods do not have bodies
+	public void fly();
+}
+

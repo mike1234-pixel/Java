d25df8b5e7e3687b2b372e0769ae0452b0d348c2
Mike <57681651+mike1234-pixel@users.noreply.github.com>
2020-05-31 17:48:25 +0100

    Instructions to create a .jar file in eclipse.

---
+++ Deploying-with-JAR-Files.java
@@ -0,0 +1,6 @@
+// In Java, we create an executable JAR (Java Archive) file in order to deploy it
+
+// To create a JAR file
+
+// Create a java project in eclipse, 
+// right click the project --> Export --> Select "Runnable JAR File" or "JAR File"
